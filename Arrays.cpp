b49a2e2b9b2acf9371717959706ef19e1cb0fb44
maxflow7 <83159018+maxflow7@users.noreply.github.com>
2026-05-21 10:11:01 +0530

    Add majority element algorithms and sorted squares function
    Implemented two solutions for finding the majority element: one using sorting and another using Boyer-Moore Voting Algorithm. Also added a function to return the squares of a sorted array in descending order.

--- Arrays.cpp
+++ Arrays.cpp
@@ -17,3 +17,102 @@ public:
         }
     }
 };
+
+
+
+****************** square of sorted array **********************************************
+    https://leetcode.com/problems/squares-of-a-sorted-array/\
+/*
+1. As we have to sort the square array in descending order, we have to check for the abs value of left and right index.
+2. Then accordingly for which the value is greater, we have to udpate the array accordingly.
+3. It's good to start from the end of index, as the array is already in ascending order.
+
+*/
+    
+
+    class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) 
+    {
+       int n = nums.size();
+
+       int left = 0;
+       int right = n-1;
+
+       vector<int>ans(n);
+
+       for(int i = n-1; i>=0 ; i--)
+       {
+
+        int square = 0;
+
+        if(abs(nums[left]) < abs(nums[right]))
+        {
+            square = nums[right];
+            right--;
+        }
+        else
+        {
+            square =nums[left];
+            left++;
+        }
+
+        ans[i] = square*square;
+
+       }
+
+       return ans;
+    }
+};
+
+
+********************** Majority Element **************************************
+/*
+
+https://leetcode.com/problems/majority-element/editorial/
+
+1. One way -> we can just sort the array and the middle element will return the majority element.
+
+Time complexity : 0(nlogn)
+Space Complexity : O(1)
+
+2. Use Boyer Moore Majority Element  :
+
+The algorithm is based on the idea that if an element occurs more than N/2 times, then all the remaining elements
+together must occur less than N/2 times.
+
+Time Complexity : O(n)
+Space Complexity : O(1)
+    */
+
+    ----------------------------- Sorting -----------------------------------------------------------------
+    class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size() / 2];
+    }
+};
+
+--------------------Boyer Moore Majority Voting Algorithm-------------------------------------------------------
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int candidate;
+
+        for (int& num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+};
+
+
+
+    
