ae2d4be2d7cf35103e0fa9224476ac02b4967837
maxflow7 <83159018+maxflow7@users.noreply.github.com>
2026-09-01 00:27:08 +0530

    Add stack-based solutions for multiple problems
    Implemented solutions for Daily Temperatures, Car Fleet, and Largest Rectangle in Histogram problems using stacks.

--- stack.cpp
+++ stack.cpp
@@ -279,3 +279,143 @@ public:
     }
 };
 
+
+----------------------------------------------------------------------- Daily Temperature ----------------------------------------------------------------------------------------------------------
+  /*
+  https://neetcode.io/problems/daily-temperatures/question
+
+  We have to find the next index which is bigger than the current index -> we keep pushing the index in stack as soon as we get the greater element than stack top element, we just store the top index and pop the 
+  top elements and store the difference between the current and top index
+  */
+
+  class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& t) 
+    {
+       int n = t.size();
+       stack<int>st;
+       vector<int>res(n,0);
+
+       for(int i = 0 ; i<t.size() ; i++)
+       {
+         while(!st.empty() and t[i] > t[st.top()])
+         {
+            int idx = st.top();
+            st.pop();
+            res[idx] = i-idx;
+         }
+         st.push(i);
+       }
+
+       return res;
+        
+    }
+};
+
+
+------------------------------------------------------------------------------- Car Fleet ----------------------------------------------------------------------------------------------------------------
+
+  /*
+  https://neetcode.io/problems/car-fleet/question
+
+  We have to think all which car will reach to the destination, at the end we have to calculate time and we have to check if we will choose the car which is taking less time than the most nearest car from the destination
+  then will definitely going to bypass the car.
+
+  Here we dont have to bypass so will first sort the cars on the basis of nearest position from the destinations then calculate the time which is taking more time will definetely not bypass.
+  
+  
+  */
+
+  class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed)
+    {
+        int n = position.size();
+        vector<pair<int, int>>cars;
+
+        for(int i = 0 ; i< n ; i++)
+        {
+            cars.push_back({position[i], speed[i]});
+        }
+
+        sort(cars.begin(), cars.end(), greater<pair<int,int>>());
+
+        stack<double>st;
+
+        for(auto c : cars)
+        {
+            auto tp = c.first;
+            auto ts = c.second;
+
+            double timetaken = (double)(target-tp)/ts;
+
+            if(st.empty() or timetaken > st.top())
+            st.push(timetaken);
+        }
+
+        return st.size();
+
+
+        
+    }
+};
+
+
+----------------------------------------------------------------------------------- Largest Rectangle in Histogram ------------------------------------------------------------------------------------------------
+
+  /*
+  https://neetcode.io/problems/largest-rectangle-in-histogram/question
+
+  1. For each element we have to keep finding the smaller elements from the left and right for the current one if any element in left or right is bigger than current then we have to pop.
+  2. Then we have to find the width and then area.
+  
+  */
+
+  class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) 
+    {
+
+        int n = heights.size();
+
+        vector<int>left(n);
+        vector<int>right(n);
+
+        stack<int>st;
+
+        for(int i = 0 ; i<n ; i++)
+        {
+            while(!st.empty() and heights[st.top()]>=heights[i])
+            st.pop();
+
+            left[i]=st.empty()?-1:st.top();
+
+            st.push(i);
+        }
+
+        while(!st.empty())
+        st.pop();
+
+        for(int i = n-1 ; i>=0 ; i--)
+        {
+            while(!st.empty() and heights[st.top()]>=heights[i])
+            st.pop();
+
+            right[i] = st.empty() ? n : st.top();
+
+            st.push(i);
+        }
+
+        int ans = 0;
+
+        for(int i = 0 ;i<n ; i++)
+        {
+            int width = right[i]-left[i]-1;
+            ans=max(ans, width*heights[i]);
+        }
+
+        return ans;
+        
+    }
+};
+
